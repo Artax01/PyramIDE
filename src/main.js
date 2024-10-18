447a7d2ed874bb6be99e2ac8a9638d2f51c6c8ad
Artax01 <158834512+Artax01@users.noreply.github.com>
2024-10-18 21:00:40 +0200

    start building homepage

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
 import './style.css'
+import './assets/css/HomePage.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
